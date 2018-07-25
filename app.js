22cf31728fd5e4bb5f2debebe463ddda19c9a8b7
Ryan Smith <rksmith369@gmail.com>
2018-07-24 19:25:03 -0700

    FIrst commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// BEFORE:
+// var my_module = require('./node_modules/my_module');
+// my_module.greet();
+// my_module.add(5,6);
+
+// After:
+var my_module = require('./my_module')();     //notice the extra invocation parentheses!
+console.log(my_module);
+my_module.greet();
+my_module.add(5,6);
